81626510154f599e5e975152ee6b3e547c8a34f0
arfenis <arfen00@gmail.com>
2021-12-22 16:15:53 -0400

    Use report type mapper on route

--- src/Traits/ReportableTypesRoutes.php
+++ src/Traits/ReportableTypesRoutes.php
@@ -5,11 +5,16 @@ declare(strict_types=1);
 namespace Kanvas\Moderation\Traits;
 
 use Baka\Contracts\Http\Api\CrudBehaviorTrait;
+use Canvas\Contracts\Controllers\ProcessOutputMapperTrait;
 use Kanvas\Moderation\Models\ReportsTypes;
+use Kanvas\Moderation\DTO\ReportsTypes as ReportsTypesDto;
+use Kanvas\Moderation\Mappers\ReportsTypes as MappersReportsTypes;
 
 trait ReportableTypesRoutes
 {
-    use CrudBehaviorTrait;
+    use CrudBehaviorTrait, ProcessOutputMapperTrait{
+        ProcessOutputMapperTrait::processOutput insteadof CrudBehaviorTrait;
+    }
 
     /**
      * Init Controller.
@@ -19,6 +24,8 @@ trait ReportableTypesRoutes
     public function onConstruct()
     {
         $this->model = new ReportsTypes();
+        $this->dto = ReportsTypesDto::class;
+        $this->dtoMapper = new MappersReportsTypes;
 
         $this->additionalSearchFields = [
             ['is_deleted', ':', 0],
